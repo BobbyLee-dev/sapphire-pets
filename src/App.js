2036a2e4a4d3f3828833a7869545e2fa426294dd
Bobby Lee <rdlee8181@gmail.com>
2019-10-27 22:37:44 -0700

    Added Search

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me</h1>
-      <Pet name='Barry' animal='Dog' breed='Corgi' />
+      {/* <Pet name='Barry' animal='Dog' breed='Corgi' />
       <Pet name='Ginger' animal='Dog' breed='Corgi' />
-      <Pet name='Freddy' animal='Cat' breed='Mix' />
+      <Pet name='Freddy' animal='Cat' breed='Mix' /> */}
+      <SearchParams />
     </div>
   );
 };
 
-render(React.createElement(App), document.getElementById('root'));
+render(<App />, document.getElementById('root'));
